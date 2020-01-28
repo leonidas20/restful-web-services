9211fac3b79e36a84d376fb6069fd75c0d4c6d3b
LT <ltheodor@hotmail.de>
2020-01-28 23:17:07 +0100

    Step 15 - Implementing Validations for RESTful Services

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,8 +2,10 @@ package com.in28minutes.rest.webservices.restfulwebservices.exception;
 
 import java.util.Date;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,24 +16,30 @@ import com.in28minutes.rest.webservices.restfulwebservices.user.UserNotFoundExce
 
 @ControllerAdvice
 @RestController
-public class CustomizedResponseEntityExceptionHandler 
-extends ResponseEntityExceptionHandler {
-	
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
 	@ExceptionHandler(Exception.class)
-	public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) 
-			throws Exception { 
-		
-		ExceptionResponse exceptionResponse = 
-				new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
-	return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
+
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+				request.getDescription(false));
+		return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-	
+
 	@ExceptionHandler(UserNotFoundException.class)
-	public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) 
-			throws Exception { 
-		
-		ExceptionResponse exceptionResponse = 
-				new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
-	return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+	public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+				request.getDescription(false));
+		return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), "Validation Failed",
+				ex.getBindingResult().getAllErrors().toString());
+		return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
 	}
 }
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -9,8 +9,10 @@ public class User {
 
 	private Integer id;
 	
+	@Size(min=2, message="Name should have at least 2 characters")
 	private String name;
 	
+	@Past
 	private Date birthDate;
 
 	public User(Integer id, String name, Date birthDate) {
