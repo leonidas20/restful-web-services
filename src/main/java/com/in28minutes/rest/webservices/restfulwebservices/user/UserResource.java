db4fbe129c467e6dd24dbbf8b2503cdd64f787d9
LT <ltheodor@hotmail.de>
2020-01-30 12:39:39 +0100

    Step 20 - Configuring Auto Generation of Swagger Documentation

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,14 +1,14 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+//import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import java.net.URI;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+//import org.springframework.hateoas.EntityModel;
+//import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +17,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import org.springframework.hateoas.Resource;
+
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+
 
 @RestController
 public class UserResource {
@@ -34,21 +44,27 @@ public class UserResource {
 	// /users/{id}
 	// retrieveUser(int id)
 	@GetMapping("/users/{id}")
-	public EntityModel retrieveUser(@PathVariable int id) {
+	public Resource retrieveUser(@PathVariable int id) {
 
 		User user = service.findOne(id);
 		if (user == null)
 			throw new UserNotFoundException("id-" + id);
-		
-		//"all-users", SERVER_PATH + "/users"
-		EntityModel<User> model = new EntityModel<>(user);
-		WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
-		model.add(linkTo.withRel("all-users"));
-		//HATEOAS
 
-		return model;
+		// "all-users", SERVER_PATH + "/users"
+
+		Resource<User> resource = new Resource<User>(user);
+		ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		resource.add(linkTo.withRel("all-users"));
+		return resource;
+
+//		EntityModel<User> model = new EntityModel<>(user);
+//		WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+//		model.add(linkTo.withRel("all-users"));
+		// HATEOAS
+
+	//	return model;
 	}
-	
+
 	@DeleteMapping("/users/{id}")
 	public void deleteUser(@PathVariable int id) {
 
@@ -66,10 +82,7 @@ public class UserResource {
 		User savedUser = service.save(user);
 		// CREATED
 		// user//{id} savedUser.getId
-		URI location = ServletUriComponentsBuilder.
-				fromCurrentRequest().
-				path("/{id}").
-				buildAndExpand(savedUser.getId())
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
 				.toUri();
 
 		return ResponseEntity.created(location).build();
