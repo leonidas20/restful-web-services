85b9945402933ec7d2f0088ae2da64d630843f73
LT <ltheodor@hotmail.de>
2020-02-03 14:38:12 +0100

    Step 33 - Creating Post Entity and Many to One Relationship with User Entity

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,10 +1,13 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -26,6 +29,9 @@ public class User {
 	@Past
 	@ApiModelProperty(notes="Birth date should be in the past")
 	private Date birthDate;
+	
+	@OneToMany (mappedBy="user")
+	private List<Post> posts;
 	
 	protected User() {
 
@@ -60,6 +66,16 @@ public class User {
 
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
+	}
+	
+	
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
